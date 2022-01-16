47e0824a851ecbe6a6b9ca72b606615d2627dece
na2hiro <a@81.la>
2022-01-16 11:35:57 +0900

    Add c1 and c2 data; Fix setting for b1 and b2

--- app/routes/junisen.$period/$class.tsx
+++ app/routes/junisen.$period/$class.tsx
@@ -4,7 +4,7 @@ import League from "~/junisen/components/League";
 import SettingContext from "~/junisen/utils/SettingContext";
 import { LoaderFunction, useLoaderData, useParams} from "remix";
 import {calcProps} from "~/junisen/utils/dataConversion";
-import {getData, setting} from "~/junisen/data";
+import {getData, getSetting} from "~/junisen/data";
 
 import styles from "~/junisen/style.css";
 import {displayClass, displayPeriod} from "~/junisen/utils/display";
@@ -22,7 +22,7 @@ export function links() {
 
 export const loader: LoaderFunction = ({params}) => {
     const data = getData(params.period!, params.class!);
-    const s = setting[params.class!];
+    const s = getSetting(params.period!, params.class!);
     if(!data || !s) {
         throw new Response("not found", {status: 404});
     }
