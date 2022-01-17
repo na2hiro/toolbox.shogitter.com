113de3cacefdc16ffcd815fb74062a6fc188cd55
na2hiro <a@81.la>
2022-01-17 14:57:32 +0900

    Auto navigate to class if a period has only one class

--- app/routes/junisen.$period.tsx
+++ app/routes/junisen.$period.tsx
@@ -1,17 +1,20 @@
-import {LoaderFunction, Outlet, useLoaderData } from "remix";
+import {LoaderFunction, Outlet, redirect, useLoaderData} from "remix";
 import LeftRightPane from "~/junisen/styled/LeftRightPane";
 import BoldNavLink from "~/junisen/styled/BoldNavLink";
 import {displayClass, displayPeriod} from "~/junisen/utils/display";
 import {getClasses, getPeriods} from "~/junisen/data";
-import { MetaFunction } from "@remix-run/react/routeModules";
+import {MetaFunction} from "@remix-run/react/routeModules";
 import {getJunisenMetas} from "~/junisen/utils/junisenMetas";
 
-type LoaderData = {classes: string[], period: string, periods: string[]};
-export const loader: LoaderFunction = ({params}) => {
+type LoaderData = { classes: string[], period: string, periods: string[] };
+export const loader: LoaderFunction = ({params, request}) => {
     const period = params.period!;
     const classes = getClasses(params.period!);
-    if(!classes) {
+    if (!classes) {
         throw new Response("not found", {status: 404});
+    }
+    if (request.url.endsWith(`/${period}`) && classes.length === 1) {
+        throw redirect(`/junisen/${period}/${classes[0]}`);
     }
     return {
         period,
@@ -27,16 +30,17 @@ export const meta: MetaFunction = () => {
 };
 
 export default function JunisenIndex() {
-    const {classes, periods ,period} = useLoaderData<LoaderData>();
+    const {classes, periods, period} = useLoaderData<LoaderData>();
     return (
         <LeftRightPane
             left={<>
                 <ul className="grow-0 list-disc ml-6">
                     {periods.map(p => <li key={p}>
                         <BoldNavLink to={`/junisen/${p}`}>{displayPeriod(p)}</BoldNavLink>
-                        {p===period && (
+                        {p === period && (
                             <ul className={"list-disc ml-6"}>
-                                {classes.map(clss => <li key={clss}><BoldNavLink to={`/junisen/${period}/${clss}`}>{displayClass(clss)}</BoldNavLink></li>)}
+                                {classes.map(clss => <li key={clss}><BoldNavLink
+                                    to={`/junisen/${period}/${clss}`}>{displayClass(clss)}</BoldNavLink></li>)}
                             </ul>
                         )}
                     </li>)}
